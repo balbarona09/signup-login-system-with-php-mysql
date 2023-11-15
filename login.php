1a74fa8d9f5021b3b4e084bbfcff49bc76e05d0a
balbarona09 <balbarona09@gmail.com>
2023-11-15 11:28:42 +0800

    add bootstrap cdn link and remove inputs placeholder

--- login.php
+++ login.php
@@ -1,16 +1,14 @@
 <?php
-
 session_start();
 require_once 'check-login.php';
 require_once 'php-actions/login.php';
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Login</title>
   </head>
   <body class="bg-dark">
@@ -25,12 +23,12 @@ require_once 'php-actions/login.php';
             <form class="form-row" method="post">
               <div class="col py-2">
                 <label class="fw-bold" for="email">Email</label>
-                <input class="form-control" type="email" name="email" id="email" placeholder="example@gmail.com"
+                <input class="form-control" type="email" name="email" id="email"
                 value = "<?php echo empty($_POST['email']) ? '' : $_POST['email'] ?>" required>
               </div>
               <div class="col py-2">
                 <label class="fw-bold" for="password">Password</label>
-                <input class="form-control" type="password" name="password" id="password" placeholder="Password123" required>
+                <input class="form-control" type="password" name="password" id="password" required>
               </div>
               <div class="col py-2">
                 <a href="#0" class="link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Forgot Password?</a>
@@ -47,7 +45,5 @@ require_once 'php-actions/login.php';
         </div>
       </div>
     </main>
-    
-    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
